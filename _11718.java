51a599c958a9345f9cf74d9a103e5e08100089e6
HelloJinoo <wkdwlsdn9637@naver.com>
2018-12-27 10:34:42 +0900

    문자열제대로알기

--- _11718.java
+++ _11718.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 
 
 public class _11718 {
-
 	public static void main(String[] args) throws IOException {
 		/*주어진 문자열에 대하여 제대로 알기*/
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,6 +12,5 @@ public class _11718 {
 		while((input = br.readLine()) != null) {
 			System.out.println(input);
 		}
-
 	}
 }
