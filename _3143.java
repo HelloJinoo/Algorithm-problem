9f95d89f6dd9b7267b0567ea0eebd2cde5624641
HelloJinoo <wkdwlsdn9637@naver.com>
2019-02-04 00:13:37 +0900

    Sw_Fastest typing

--- _3143.java
+++ _3143.java
@@ -1,3 +1,7 @@
+/* Sw Expert Academy 3143
+ * 가장 빠른 문자열 타이핑
+ * 전체문자열을 타이핑 하기 위해 키를 눌러야 하는 횟수의 최솟값 , 하나의 키워드 존재
+ */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,34 +14,24 @@ public class _3143 {
 		
 		for(int test=1; test<=test_case; test++) {
 			String input[] = br.readLine().split(" ");
-			String text = input[0];
-			String one_text = input[1];
-			int count=0;
-	
-			for(int i=0; i<text.length(); i++) {
-				if(text.charAt(i) == one_text.charAt(0)) {
-					for(int j=1; j<one_text.length(); j++) {
-						if(text.charAt(i+j) != one_text.charAt(j)){
-							count++;
-							i += j;
-							break;
-						}
-						else if(j == one_text.length()-1) {
-							count++;
-							i = i+j;
-						}
-					}
-					
-				}
-				else {
-					count++;
-				}
+			String text = input[0];	//입력문자
+			String one_text = input[1];  //하나의 키워드
 			
+			String sp[] = text.split(one_text);
+			
+			int text_s = 0;
+			for(int i=0; i<sp.length; i++) {
+				text_s += sp[i].length();	//하나씩 눌러야하는 글자들
 			}
-			System.out.println("#"+test+" "+count);
+			
+			int extra_s = text.length() - text_s; 
+			int count = text_s;	
+			count += extra_s/one_text.length();	//키워드 입력 횟수 
+			
+			System.out.println("#"+test+" "+ count);
 			
 		}
+			
 		
 	}
-
 }
